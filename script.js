94551658b2bf92831bddb1e1c64cc5ba0eef8d3d
Preetinder0401 <preetinder0548.be20@chitkara.edu.in>
2022-08-11 17:35:38 +0530

    Third commit before contact section

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.querySelector('#btn-search-icon').addEventListener('click', () => {
     const xhr = new XMLHttpRequest()
-    const value = document.querySelector('search-input').value
+    const value = document.querySelector('#search-input').value
+
+    console.log(value)
 
     let url = "https://jsonplaceholder.typicode.com/photos?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09"
     
@@ -9,22 +11,22 @@ document.querySelector('#btn-search-icon').addEventListener('click', () => {
     xhr.onreadystatechange = () => {
         if(xhr.status === 200 && xhr.readyState === 4) {
             const response = JSON.parse(xhr.responseText)
-            
-            let output = ''
-            for(let i=1; i <= response.length; i++) {
-                if(value == response[i].albumId){
-                    for(let j=1; j <= 50; j++){
+            for(let i=0; i < response.length; i++) {
+                if(response[i].albumId == value){
+                    let output = ``
+                    for(let j=0; j < 50; j++){
+                        console.log(j)
                         output += `
-                            <div style="width: 22%; margin: 1.2em;">
+                            <div style="width: 22.75%; margin: 1.2em;">
                                 <img style="width: 100%;" src=${response[j].url} />
                                 <h4>${response[j].title}</h4>
                             </div>
                         `
                     }
+                    document.querySelector('#videos').innerHTML = output
+                    break
                 }
             }
-
-            document.querySelector('#videos').innerHTML = output
         }
     }
 
